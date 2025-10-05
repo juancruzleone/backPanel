f9e71eb5c99d25893266540c7223564ae61fdd6c
Juan cruz Leone <juancruzleone@outlook.com>
2025-10-05 13:35:27 -0300

    Feat: Modify asset schema

--- schemas/asset.schema.js
+++ schemas/asset.schema.js
@@ -3,11 +3,17 @@ import yup from "yup"
 const assetSchemaCreate = yup.object({
   nombre: yup.string().required("El nombre es obligatorio"),
   templateId: yup.string().required("La plantilla de formulario es obligatoria"),
+  marca: yup.string().optional(),
+  modelo: yup.string().optional(),
+  numeroSerie: yup.string().optional(),
 })
 
 const assetSchemaPatch = yup.object({
   nombre: yup.string(),
   templateId: yup.string(),
+  marca: yup.string().optional(),
+  modelo: yup.string().optional(),
+  numeroSerie: yup.string().optional(),
 })
 
 export { assetSchemaCreate, assetSchemaPatch }
